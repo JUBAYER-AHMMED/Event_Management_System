64896096b0f51889e93b1dc51aebf3c7792a162a
JUBAYER AHMMED <jubayerahmmed105@gmail.com>
2026-01-01 01:43:18 +0600

    updated requirements txt file.

--- users/forms.py
+++ users/forms.py
@@ -10,7 +10,7 @@ from django.contrib.auth.forms import AuthenticationForm
 class StyledFormMixin2:
     default_classes = (
         "border-2 border-gray-300 "
-        "text-white bg-slate-700 "
+        "bg-slate-700 "
         "p-1 w-full rounded-lg shadow-sm "
         "focus:border-rose-500 focus:ring-rose-500"
     )
@@ -73,10 +73,7 @@ class StyledFormMixin4:
             field.widget.attrs.update({
                 "class": (
                     "w-full px-3 py-2 rounded-md "
-                    "border border-gray-300 "
-                    "bg-white text-gray-900 "
-                    "focus:outline-none focus:ring-2 focus:ring-indigo-500 "
-                    "focus:border-indigo-500"
+                    "border border-gray-300 text-black bg-slate-500"
                 )
             })
 
