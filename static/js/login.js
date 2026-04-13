fe5457b8cf4c6c1fb3307e3d58f80f9d19f9c46c
Jubayer <jubayerahmmed105@gmail.com>
2026-04-13 14:17:05 +0600

    updated the effect on btn click of authpage

--- static/js/login.js
+++ static/js/login.js
@@ -8,3 +8,18 @@ registerBtn.addEventListener('click', () =>{
 loginBtn.addEventListener('click', () =>{
     container.classList.remove("active");
 });
+
+
+// Sign In button loading state
+    document.querySelector('.sign-in form').addEventListener('submit', function() {
+        const btn = this.querySelector('button');
+        btn.textContent = 'Signing In...';
+        btn.disabled = true;
+    });
+
+    // Sign Up button loading state
+    document.querySelector('.sign-up form').addEventListener('submit', function() {
+        const btn = this.querySelector('button');
+        btn.textContent = 'Creating Account...';
+        btn.disabled = true;
+    });
